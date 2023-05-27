a2df8166609a6413009c6863a49311c0caf98d5e
hari-prasath-2003 <111623200+hari-prasath-2003@users.noreply.github.com>
2023-05-27 22:16:34 +0530

    final-commit

---
+++ firebase.js
@@ -0,0 +1,14 @@
+const firebase = require("firebase/compat/app");
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAqXW-B2AeK9QW7RAPPuaJd7Dj4o0Wa-D8",
+  authDomain: "whatsappautoimg.firebaseapp.com",
+  projectId: "whatsappautoimg",
+  storageBucket: "whatsappautoimg.appspot.com",
+  messagingSenderId: "946245872996",
+  appId: "1:946245872996:web:02a9ed7d529be540365e71",
+  measurementId: "G-KE3EV2WH6L",
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
---
+++ index.js
@@ -0,0 +1,50 @@
+const qrcode = require("qrcode-terminal");
+const { Client, MessageMedia } = require("whatsapp-web.js");
+const firebase = require("firebase/compat/app");
+require("./firebase.js");
+require("firebase/compat/storage");
+require("firebase/compat/firestore");
+const cron = require("node-cron");
+const moment = require("moment");
+
+let targetGroup;
+
+function sendMsg() {
+  const filename = moment().format("L").replaceAll("/", ".");
+  const storageRef = firebase.storage().ref().child(`${filename}.jpg`);
+  // Get the download URL
+  storageRef
+    .getDownloadURL()
+    .then((downloadURL) => {
+      return MessageMedia.fromUrl(downloadURL);
+    })
+    .then((media) => {
+      client.sendMessage(targetGroup.id._serialized, media);
+      console.log("msg sent");
+    });
+}
+
+// Use the saved values
+const client = new Client({});
+
+// Save session values to the file upon successful auth
+client.on("authenticated", (session) => {
+  console.log(session);
+});
+
+client.on("qr", (qr) => {
+  qrcode.generate(qr, { small: true });
+});
+
+client.on("ready", async () => {
+  console.log("client ready");
+  const groups = await client.getChats();
+  targetGroup = groups.find((group) => group.name === "R k Illam");
+  sendMsg();
+});
+
+cron.schedule("00 06 * * *", () => {
+  sendMsg();
+});
+
+client.initialize();
